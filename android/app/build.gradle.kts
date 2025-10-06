104e9e158b9ae832e1dbfbf49272ba6f8feb5937
Matty Jones <mattyl.jones@dunelm.org.uk>
2025-10-06 22:08:10 +0000

    Softcoded the server details to the client app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,6 +20,14 @@ android {
         vectorDrawables.useSupportLibrary = true
     }
     buildTypes {
+	getByName("debug") {
+	    buildConfigField("String", "BASE_URL", "\"http://192.168.86.3:8088/\"")
+	}
+	getByName("release") {
+	    // keep same or point to whatever you use in prod
+	    buildConfigField("String", "BASE_URL", "\"http://192.168.86.3:8088/\"")
+	    // shrinker/proguard rules are fine; BASE_URL is inlined
+	}
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
