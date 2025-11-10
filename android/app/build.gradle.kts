784fe45d6c89ef72d5ca05a2f019abe1bfb04fa9
Matty Jones <mattyl.jones@dunelm.org.uk>
2025-11-10 11:33:48 -0700

    Fixed the application release script to use Java 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.contentedest.baby"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         buildConfigField("String", "BASE_URL", "\"http://192.168.86.3:8005/\"")
@@ -29,11 +29,9 @@ android {
 	    // keep same or point to whatever you use in prod
 	    buildConfigField("String", "BASE_URL", "\"http://192.168.86.3:8005/\"")
 	    // shrinker/proguard rules are fine; BASE_URL is inlined
+	    isMinifyEnabled = false
+	    proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
 	}
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
