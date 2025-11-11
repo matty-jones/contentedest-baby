3fa182db38e3504f2fca9db96fca10c011fc4525
Matty Jones <mattyl.jones@dunelm.org.uk>
2025-11-11 20:25:57 +0000

    Latest build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.contentedest.baby"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         buildConfigField("String", "BASE_URL", "\"http://192.168.86.3:8005/\"")
